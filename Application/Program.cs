478b2e3ce6b370e3c0346271f074b6a6c864bfb2
Oskar <hamerlao@gmail.com>
2025-01-05 13:18:59 +0100

    refactored the code to be easier to read and maintain

--- Application/Program.cs
+++ Application/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Commands;
 using Application.Filters;
+using System.Net;
 
 namespace Application
 {
@@ -7,25 +8,15 @@ namespace Application
     {
         static async Task Main(string[] args)
         {
-            
             try
             {
+                ValidateArguments(args);
+
                 string url = args[1];
-                byte[] fileData;
-                string contentType;
+                var response = await GetFileAsync(url);
+                byte[] fileData = await response.Content.ReadAsByteArrayAsync();
+                string contentType = response.Content.Headers.ContentType.MediaType;
 
-                using (HttpClient client = new HttpClient())
-                {
-                    HttpResponseMessage response = await client.GetAsync(url);
-
-                    if (!(response.StatusCode == System.Net.HttpStatusCode.OK))
-                        throw new Exception("Cannot get file");
-
-
-                    fileData = await response.Content.ReadAsByteArrayAsync();
-                    contentType = response.Content.Headers.ContentType.MediaType;
-
-                }
 
                 var filter = FilterFactory.Create(contentType);
                 var result = filter.Handle(fileData);
@@ -36,13 +27,35 @@ namespace Application
                 commandService.GenerateResults();
 
 
-            }catch(Exception exception)
+            }
+            catch (Exception exception)
             {
                 Console.WriteLine($"Error: {exception.Message}.");
 
             }
-           
 
         }
+
+
+        private static void ValidateArguments(string[] args)
+        {
+            if (args.Length < 2 || string.IsNullOrEmpty(args.ElementAtOrDefault(1)))
+                throw new Exception("Cannot process the file");
+        }
+
+        private static async Task<HttpResponseMessage> GetFileAsync(string url)
+        {
+            using var client = new HttpClient();
+            var response = await client.GetAsync(url);
+
+            if (response.StatusCode != HttpStatusCode.OK)
+                throw new Exception("Cannot get file");
+
+            return response;
+        }
+
+
+
+
     }
 }
